bd4034aab603242a3aee7515dd476d020bfb51f4
Valery Tschopp <valery.tschopp@switch.ch>
2013-09-05 11:21:55 +0200

    Small refactoring. Better INFO log messages.

--- src/main/java/org/glite/authz/pdp/config/PDPIniConfigurationParser.java
+++ src/main/java/org/glite/authz/pdp/config/PDPIniConfigurationParser.java
@@ -44,54 +44,89 @@ import org.slf4j.LoggerFactory;
 
 /** Configuration parser for PDP INI configuration files. */
 @ThreadSafe
-public class PDPIniConfigurationParser extends AbstractIniServiceConfigurationParser<PDPConfiguration> {
+public class PDPIniConfigurationParser extends
+        AbstractIniServiceConfigurationParser<PDPConfiguration> {
 
-    /** The name of the {@value} INI header which contains the property for configuring the PAP interaction. */
-    public static final String POLICY_SECTION_HEADER = "POLICY";
+    /**
+     * The name of the {@value} INI header which contains the property for
+     * configuring the PAP interaction.
+     */
+    public static final String POLICY_SECTION_HEADER= "POLICY";
 
-    /** The name of the {@value} property which gives the space-delimited PAP endpoint URLs. */
-    public static final String PAP_PROP = "paps";
+    /**
+     * The name of the {@value} property which gives the space-delimited PAP
+     * endpoint URLs.
+     */
+    public static final String PAP_PROP= "paps";
 
-    /** The name of the {@value} property which indicates length of time, in minutes, a policy will be cached. */
-    public static final String POLICY_RETENTION_PROP = "retentionInterval";
+    /**
+     * The name of the {@value} property which indicates length of time, in
+     * minutes, a policy will be cached.
+     */
+    public static final String POLICY_RETENTION_PROP= "retentionInterval";
 
-    /** The name of the {@value} property which indicates the allowed clock skew, in seconds. */
-    public static final String CLOCK_SKEW_PROP = "clockSkew";
+    /**
+     * The name of the {@value} property which indicates the allowed clock skew,
+     * in seconds.
+     */
+    public static final String CLOCK_SKEW_PROP= "clockSkew";
 
-    /** The name of the {@value} property which indicates the maximum validity of messages, in seconds. */
-    public static final String MESSAGE_VALIDITY_PROP = "messageValidityPeriod";
+    /**
+     * The name of the {@value} property which indicates the maximum validity of
+     * messages, in seconds.
+     */
+    public static final String MESSAGE_VALIDITY_PROP= "messageValidityPeriod";
 
-    /** Default value of the {@value AbstractIniServiceConfigurationParser#PORT_PROP} property, {@value} . */
-    public static final int DEFAULT_PORT = 8152;
+    /**
+     * Default value of the
+     * {@value AbstractIniServiceConfigurationParser#PORT_PROP} property, *
+     * {@value} .
+     */
+    public static final int DEFAULT_PORT= 8152;
 
-    /** Default value of the {@value AbstractIniServiceConfigurationParser#ADMIN_PORT_PROP} property, {@value} . */
-    public static final int DEFAULT_ADMIN_PORT = 8153;
+    /**
+     * Default value of the
+     * {@value AbstractIniServiceConfigurationParser#ADMIN_PORT_PROP} property,
+     * * {@value} .
+     */
+    public static final int DEFAULT_ADMIN_PORT= 8153;
 
-    /** Default value of the {@value #POLICY_RETENTION_PROP} property, {@value} minutes. */
-    public static final int DEFAULT_POLICY_RETENTION = 240;
+    /**
+     * Default value of the {@value #POLICY_RETENTION_PROP} property, {@value}
+     * minutes.
+     */
+    public static final int DEFAULT_POLICY_RETENTION= 240;
 
-    /** Default value of the {@value #CLOCK_SKEW_PROP} property, {@value} seconds. */
-    public static final int DEFAULT_CLOCK_SKEW = 30;
+    /**
+     * Default value of the {@value #CLOCK_SKEW_PROP} property, {@value}
+     * seconds.
+     */
+    public static final int DEFAULT_CLOCK_SKEW= 30;
 
-    /** Default value of the {@value #MESSAGE_VALIDITY_PROP} property, {@value} seconds. */
-    public static final int DEFAULT_MESSAGE_VALIDITY = 300;
+    /**
+     * Default value of the {@value #MESSAGE_VALIDITY_PROP} property, {@value}
+     * seconds.
+     */
+    public static final int DEFAULT_MESSAGE_VALIDITY= 300;
 
     /** Class logger. */
-    private final Logger log = LoggerFactory.getLogger(PDPIniConfigurationParser.class);
+    private final Logger log= LoggerFactory.getLogger(PDPIniConfigurationParser.class);
 
     /**
      * {@inheritDoc}
      * 
-     * @return the port value, or the default port {@value #DEFAULT_PORT} if it is not set
+     * @return the port value, or the default port {@value #DEFAULT_PORT} if it
+     *         is not set
      */
     protected int getPort(Ini.Section configSection) {
         return IniConfigUtil.getInt(configSection, PORT_PROP, DEFAULT_PORT, 1, 65535);
     }
 
-    /** 
+    /**
      * {@inheritDoc}
      * 
-     * @return the admin port value, or the default admin port {@value #DEFAULT_ADMIN_PORT} if it is not set
+     * @return the admin port value, or the default admin port
+     *         {@value #DEFAULT_ADMIN_PORT} if it is not set
      */
     protected int getAdminPort(Ini.Section configSection) {
         return IniConfigUtil.getInt(configSection, ADMIN_PORT_PROP, DEFAULT_ADMIN_PORT, 1, 65535);
@@ -100,15 +135,18 @@ public class PDPIniConfigurationParser extends AbstractIniServiceConfigurationPa
     /**
      * Parses a configuration.
      * 
-     * @param iniReader INI to parse
+     * @param iniReader
+     *            INI to parse
      * 
      * @return the daemon configuration
      * 
-     * @throws ConfigurationException thrown if there is a problem configuring the system
+     * @throws ConfigurationException
+     *             thrown if there is a problem configuring the system
      */
-    protected PDPConfiguration parseIni(Reader iniReader) throws ConfigurationException {
+    protected PDPConfiguration parseIni(Reader iniReader)
+            throws ConfigurationException {
 
-        Ini pdpIni = new Ini();
+        Ini pdpIni= new Ini();
         try {
             log.info("Loading and parsing INI configuration file");
             pdpIni.load(iniReader);
@@ -117,90 +155,112 @@ public class PDPIniConfigurationParser extends AbstractIniServiceConfigurationPa
             throw new ConfigurationException("Unable to parse INI configuration file", e);
         }
 
-        PDPConfigurationBuilder configBuilder = new PDPConfigurationBuilder();
+        PDPConfigurationBuilder configBuilder= new PDPConfigurationBuilder();
 
         log.info("Processing PDP {} configuration section", SECURITY_SECTION_HEADER);
         processSecuritySection(pdpIni, configBuilder);
 
         log.info("Processing PDP {} configuration section", SERVICE_SECTION_HEADER);
-        processServiceSection(pdpIni, configBuilder);
-
-        Ini.Section serviceSection = pdpIni.get(SERVICE_SECTION_HEADER);
-
-        List<PolicyInformationPoint> pips = IniPIPConfigurationParserHelper.processPolicyInformationPoints(pdpIni,
-                serviceSection, configBuilder);
-        log.info("Total policy information points: {}", pips.size());
-        configBuilder.getPolicyInformationPoints().addAll(pips);
-
-        ObligationService service = IniOHConfigurationParserHelper.processObligationHandlers(pdpIni, serviceSection,
-                configBuilder);
-        log.info("Total obligation handlers: {}", service.getObligationHandlers().size());
-        configBuilder.setObligationService(service);
+        processPDPServiceSection(pdpIni, configBuilder);
 
         log.info("Processing PDP {} configuration section", POLICY_SECTION_HEADER);
-        processPAPConfiguration(pdpIni, configBuilder);
+        processPDPPolicySection(pdpIni, configBuilder);
 
         return configBuilder.build();
     }
 
     /**
-     * Process the PAP section of the INI file.
+     * Process the PDP SERVICE section of the ini file. Checks and parse PIPs
+     * and OHs config.
      * 
-     * @param iniFile ini file to process
-     * @param configBuilder the PDP configuration builder
-     * 
-     * @throws ConfigurationException thrown if there is a problem reading the INI configuration
+     * @param iniFile
+     *            the PDP ini file
+     * @param configBuilder
+     *            the PDP configuration builder
+     * @throws ConfigurationException
      */
-    protected void processPAPConfiguration(Ini iniFile, PDPConfigurationBuilder configBuilder)
+    protected void processPDPServiceSection(Ini iniFile,
+                                            PDPConfigurationBuilder configBuilder)
             throws ConfigurationException {
-        Ini.Section configSection = iniFile.get(POLICY_SECTION_HEADER);
+        
+        // process the generic config
+        processServiceSection(iniFile, configBuilder);
+        
+        // process the PIPs and OHs if any
+        Ini.Section serviceSection= iniFile.get(SERVICE_SECTION_HEADER);
+
+        List<PolicyInformationPoint> pips= IniPIPConfigurationParserHelper.processPolicyInformationPoints(iniFile, serviceSection, configBuilder);
+        configBuilder.getPolicyInformationPoints().addAll(pips);
+        log.info("{}: Total Policy Information Points: {}", serviceSection.getName(), pips.size());
+
+        ObligationService service= IniOHConfigurationParserHelper.processObligationHandlers(iniFile, serviceSection, configBuilder);
+        configBuilder.setObligationService(service);
+        log.info("{}: Total Obligation Handlers: {}", serviceSection.getName(), service.getObligationHandlers().size());
+    }
+
+    /**
+     * Process the POLICY (PAP) section of the INI file.
+     * 
+     * @param iniFile
+     *            ini file to process
+     * @param configBuilder
+     *            the PDP configuration builder
+     * 
+     * @throws ConfigurationException
+     *             thrown if there is a problem reading the INI configuration
+     */
+    protected void processPDPPolicySection(Ini iniFile,
+                                           PDPConfigurationBuilder configBuilder)
+            throws ConfigurationException {
+        Ini.Section configSection= iniFile.get(POLICY_SECTION_HEADER);
         if (configSection == null) {
-            String errorMsg = "PDP INI configuration does not contain the rquired '" + POLICY_SECTION_HEADER
-                    + "' INI section";
+            String errorMsg= "PDP INI configuration does not contain the rquired '"
+                    + POLICY_SECTION_HEADER + "' INI section";
             log.error(errorMsg);
             throw new ConfigurationException(errorMsg);
         }
         String name= configSection.getName();
 
-        String papsStr = IniConfigUtil.getString(configSection, PAP_PROP);
+        String papsStr= IniConfigUtil.getString(configSection, PAP_PROP);
         log.info("{}: PAP endpoints: {}", name, papsStr);
-        StringTokenizer paps = new StringTokenizer(configSection.get(PAP_PROP), " ");
+        StringTokenizer paps= new StringTokenizer(configSection.get(PAP_PROP), " ");
         while (paps.hasMoreTokens()) {
             configBuilder.getPAPEndpoints().add(paps.nextToken());
         }
 
-        int policyRetentionInterval = IniConfigUtil.getInt(configSection, POLICY_RETENTION_PROP,
-                DEFAULT_POLICY_RETENTION, 1, Integer.MAX_VALUE);
+        int policyRetentionInterval= IniConfigUtil.getInt(configSection, POLICY_RETENTION_PROP, DEFAULT_POLICY_RETENTION, 1, Integer.MAX_VALUE);
         log.info("{}: Policy retention interval: {} minutes", name, policyRetentionInterval);
         configBuilder.setPolicyRetentionInterval(policyRetentionInterval);
 
-        BasicParserPool parserPool = new BasicParserPool();
+        BasicParserPool parserPool= new BasicParserPool();
         parserPool.setMaxPoolSize(1);
 
-//        try {
-            HttpClientBuilder soapClientBuilder = buildSOAPClientBuilder(configSection, configBuilder.getKeyManager(),
-                    configBuilder.getTrustManager());
-            configBuilder.setSoapClient(new HttpSOAPClient(soapClientBuilder.buildClient(), parserPool));
-//        } catch (Exception e) {
-//            throw new ConfigurationException("Unable to read X.509 trust material information.", e);
-//        }
+        // try {
+        HttpClientBuilder soapClientBuilder= buildSOAPClientBuilder(configSection, configBuilder.getKeyManager(), configBuilder.getTrustManager());
+        configBuilder.setSoapClient(new HttpSOAPClient(soapClientBuilder.buildClient(), parserPool));
+        // } catch (Exception e) {
+        // throw new
+        // ConfigurationException("Unable to read X.509 trust material information.",
+        // e);
+        // }
     }
 
     /**
-     * Creates the message security policy from the information in the given INI configuration section.
+     * Creates the message security policy from the information in the given INI
+     * configuration section.
      * 
-     * @param configSection INI section containing configuration information
+     * @param configSection
+     *            INI section containing configuration information
      * 
      * @return the generated security policy
      */
     protected SecurityPolicy buildSecurityPolicy(Ini.Section configSection) {
-        BasicSecurityPolicy securityPolicy = new BasicSecurityPolicy();
+        BasicSecurityPolicy securityPolicy= new BasicSecurityPolicy();
 
-        int clockSkew = IniConfigUtil.getInt(configSection, CLOCK_SKEW_PROP, DEFAULT_CLOCK_SKEW, 1, Integer.MAX_VALUE);
-        int messageValidityPeriod = IniConfigUtil.getInt(configSection, MESSAGE_VALIDITY_PROP,
-                DEFAULT_MESSAGE_VALIDITY, 1, Integer.MAX_VALUE);
+        int clockSkew= IniConfigUtil.getInt(configSection, CLOCK_SKEW_PROP, DEFAULT_CLOCK_SKEW, 1, Integer.MAX_VALUE);
+        int messageValidityPeriod= IniConfigUtil.getInt(configSection, MESSAGE_VALIDITY_PROP, DEFAULT_MESSAGE_VALIDITY, 1, Integer.MAX_VALUE);
         log.info("SAML message validating: {} seconds with a {} second clock skew", messageValidityPeriod, clockSkew);
-        IssueInstantRule issueInstant = new IssueInstantRule(clockSkew, messageValidityPeriod);
+        IssueInstantRule issueInstant= new IssueInstantRule(clockSkew, messageValidityPeriod);
         securityPolicy.getPolicyRules().add(issueInstant);
 
         // TODO client cert
